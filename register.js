3de9b63db68e484976a19eb3827e93908981b4eb
MarieWS <mwosaz@gmail.com>
2025-01-16 21:00:08 +0100

    Update register.js

--- register.js
+++ register.js
@@ -1,48 +1,39 @@
-
-document.querySelector("#login-register").addEventListener("click", () => {
-    const formContainer = document.querySelector(".form_container");
-    formContainer.classList.toggle("active");
-});
-
 const form = document.getElementById('register-form');
 
 form.addEventListener('submit', function (event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const formData = new FormData(form);
-    const data = Object.fromEntries(formData);
-    const isLogin = formContainer.classList.contains("active");
+  const formData = new FormData(form);
+  const data = Object.fromEntries(formData);
 
-    const options = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'accept': 'application/json'
-        },
-        body: JSON.stringify(data),
-    };
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'accept': 'application/json'
+    },
+    body: JSON.stringify(data),
+  };
 
-    const endpoint = isLogin ? 'https://delish-nutrio.onrender.com/api/login' : 'https://delish-nutrio.onrender.com/api/register';
+  const endpoint = 'https://delish-nutrio.onrender.com/api/register'; // Assuming this is the registration endpoint
 
-    fetch(endpoint, options)
-        .then(response => {
-            if (!response.ok) {
-                return response.text().then(text => {
-                    console.log(text);
-                    throw new Error(`HTTP error! Status: ${response.status}`);
-                });
-            }
-            return response.json();
-        })
-        .then(data => {
-            console.log('Success:', data);
-            if (isLogin) {
-                window.location.href = 'dashboard.html';
-            } else {
-                window.location.href = 'verifyEmail.html';
-            }
-        })
-        .catch(error => {
-            console.error('Error:', error);
+  fetch(endpoint, options)
+    .then(response => {
+      if (!response.ok) {
+        return response.text().then(text => {
+          console.log(text);
+          throw new Error(`HTTP error! Status: ${response.status}`);
         });
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log('Success:', data);
+      // Handle successful registration (e.g., redirect to verification page)
+      window.location.href = 'verifyEmail.html';
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      // Display an error message to the user
+    });
 });
